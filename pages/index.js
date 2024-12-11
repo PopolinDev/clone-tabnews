f15df324433422422a53a17b1e3a8cfad7efb402
PopolinDev <82101252+PopolinDev@users.noreply.github.com>
2024-12-11 00:42:19 +0000

    adiciona um link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 function Home() {
-    return <h1>Fernanda Barcelos eu te amo. Se você me ama, me dá um booster do pokemon hihi 😊</h1>
+    return (
+        <h1>
+            Fernanda Barcelos eu te amo. Se você me ama, me dá um booster do Pokémon hihi 😊 
+            <br />
+            <a href="https://www.rihappy.com.br/cartas-pokemon-blister-quadruplo-ev07-copag/p" target="_blank" rel="noopener noreferrer">
+                Clique aqui para comprar o booster!
+            </a>
+        </h1>
+    );
 }
 export default Home;
