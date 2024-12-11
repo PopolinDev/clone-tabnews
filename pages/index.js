e5e943cfb0f8da920b1ec5693d3d9fe1105eabd2
PopolinDev <82101252+PopolinDev@users.noreply.github.com>
2024-12-11 00:31:07 +0000

    modifica a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Fernanda Barcelos eu te amo. Se você me ama, me dá um booster do pokemon hihi</h1>
+    return <h1>Fernanda Barcelos eu te amo. Se você me ama, me dá um booster do pokemon hihi 😊</h1>
 }
 export default Home;
